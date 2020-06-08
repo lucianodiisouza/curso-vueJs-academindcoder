05dc121330e0d2cfba475dd83a517cc4121d1bd8
Luciano dii Souza <lucianodiisouza@hotmail.com>
2020-06-08 19:56:52 -0300

    First challenge: DONE

---
+++ cap1/dom/desafio-01/app.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: "#desafio",
+  data: {
+    nome: "Luciano",
+    idade: "29",
+    linkImg:
+      "https://img.ibxk.com.br/ns/rexposta/2019/01/13/13185307247502.jpg?watermark=neaki&w=600",
+  },
+  methods: {
+    numero: function () {
+      return Math.random();
+    },
+  },
+});
